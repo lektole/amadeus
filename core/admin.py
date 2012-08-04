c7a32f67d4df083c0eadb3c2488842ac4e4a6abe
Alexsander Toledo <lektole@gmail.com>
2012-08-04 15:51:15 -0300

    novo teste

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from core.models import User
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'email', 'sex', 'birthday', 'profileImage', 'createdAt')
-
-admin.site.register(User, UserAdmin)
--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('core.views',
-    url(r'^$', 'user', name='user'),
-    url(r'^(\d+)/sucesso/$', 'success', name='success'),
-)
